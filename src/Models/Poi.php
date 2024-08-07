7bcbbeb5d96b6c273f8a00e1e31780f892f507e2
DProgrammerOf <renanvollenghaupt@icloud.com>
2024-08-07 18:04:01 -0300

    Remove files not needed

--- src/Models/Poi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    namespace Models;
-
-    class Poi extends \Illuminate\Database\Eloquent\Model
-    {
-        protected $table = 'poi';
-        protected $connection = 'tracker';
-    }
